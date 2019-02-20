a8c5b46139a3c82d7783d0c466b909af40680bdf
Jenny Chu <jchudesign0930@gmail.com>
2019-02-20 04:51:05 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,12 +1,18 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {:name => "Lady Montague",
-      :age => "54"
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
 
   
 end
